60a7c78ecbe9bf83f9610d5d12eca8282ec500f7
karukurobert <karukurobert@gmail.com>
2021-07-31 11:18:43 +0300

    third commit

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise01
@@ -10,6 +11,34 @@ namespace Exercise01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The default regular expression checks for at least on digit");
+            Console.Write("Please Enter a regular expression (or press enter to use the default): ");
+            var inputRE = Console.ReadLine();
+            Console.Write("Please Enter an input to compare with: ");
+            var inputCW = Console.ReadLine();
+             
+            Console.Write(string.Format("Your input {0} matches {1} {2}", inputCW, inputRE, DoComparison(inputRE, inputCW)));
+            Console.Write("Press <Enter>  to exit... ");
+        }
+        static bool DoComparison(string inputRegExp, string inputCompareWith)
+        { 
+            var myReg =  "[0-9]";
+            try
+            {
+
+                if (!string.IsNullOrWhiteSpace(inputRegExp))
+                    myReg = inputRegExp;
+
+                var isMatch = Regex.IsMatch(inputCompareWith, myReg);
+                 
+                return isMatch;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
         }
     }
 }
